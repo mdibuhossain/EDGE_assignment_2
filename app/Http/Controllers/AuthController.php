2ed444cad99a094ed5ba634078103e1787fe3953
Ibrahim <ibuhossain3@gmail.com>
2024-06-06 15:07:52 +0600

    updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -9,5 +10,20 @@ class AuthController extends Controller
     public function getToken()
     {
         return csrf_token();
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        $findUser = User::where("email", $credentials["email"])->where("password", $credentials["password"])->first();
+        echo "<pre>";
+        print_r($findUser->toArray());
+        echo "</pre>";
+
+        // if (Auth::attempt($credentials)) {
+        //     $request->session()->regenerate();
+        //     return response()->json(['message' => 'Login success']);
+        // }
+        // return response()->json(['message' => 'Login failed'], 401);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,6 @@ Route::post('/api/products/', [ProductController::class, 'storeProduct']);
 Route::put('/api/products/{id}', [ProductController::class, 'updateProduct']);
 Route::get('/api/products/{id}', [ProductController::class, 'getProductById']);
 Route::delete('/api/products/{id}', [ProductController::class, 'destroyProduct']);
+
+
+Route::get('/api/login', [AuthController::class, 'login']);
