a2340967b103456f43de131ecb660e27f4fdd55b
Ibrahim <ibuhossain3@gmail.com>
2024-06-06 14:45:25 +0600

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function getProducts()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    public function getProductById($id)
+    {
+        $product = Product::find($id);
+        return response()->json($product);
+    }
+
+    public function storeProduct(Request $request)
+    {
+        $product = Product::create($request->all());
+        $product->save();
+        return response()->json($product);
+    }
+
+    public function updateProduct(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->update($request->all());
+        return response()->json($product);
+    }
+
+    public function destroyProduct($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        return response()->json($product);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/api', [AuthController::class, 'getToken']);
+
+Route::get('/api/products', [ProductController::class, 'getProducts']);
+Route::post('/api/products/', [ProductController::class, 'storeProduct']);
+Route::put('/api/products/{id}', [ProductController::class, 'updateProduct']);
+Route::get('/api/products/{id}', [ProductController::class, 'getProductById']);
+Route::delete('/api/products/{id}', [ProductController::class, 'destroyProduct']);
